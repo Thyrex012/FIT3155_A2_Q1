162e71e0736ec85c86e53675ae1f2c516a82d7eb
Thyrex12 <ekrithyreachlay012@gmail.com>
2026-05-10 23:41:14 +1000

    Implemented rule 2 alternate and also created helper functions to create leaf nodes and internal nodes as they have different end points

--- a2q1.py
+++ a2q1.py
@@ -1,13 +1,57 @@
+class End:
+    def __init__(self, val=0):
+        self.val = val
+
 class Node:
-    def __init__(self, start:int, end:int, suffix_link = None, isLeaf = False):
-        self.start = start
-        self.end = end
-        self.suffix_link = suffix_link
+    def __init__(self, start, end, suffix_link = None, isLeaf = False):
+        self.start: int = start
+        self.end: int | End = end
+        self.suffix_link: Node = suffix_link
         self.children = [None for _ in range(126-36+1)]
-        self.isLeaf = isLeaf
+        self.isLeaf: bool = isLeaf
 
 class Ukkonen_algorithm:
     def __init__(self):
-        pass
+        self.root = Node(-1, -1, isLeaf = False)
+        self.root.suffix_link = self.root 
+        self.ALPHABET_START = 36
+        self.ALPHABET_END = 126
+        self.global_end = End(0)
+        self.active_node: Node = self.root
+        self.remainder: tuple[int, int] = None
+        self.pending_node: Node = None # When a new node is created in extension j it's suffix link 
+                                      # will be resolved in extension j+1
 
-    def add_edge()
+    def construct_suffix_array(self, txt):
+        txt_with_dollar = txt + '$'
+        phase_i = 0
+        extension_j = 0
+        while phase_i >= len(txt_with_dollar):
+            while extension_j <= phase_i:
+                return
+            
+    def rule_two_regular(active_node, ):
+        return
+    
+    def rule_two_alternate(self, active_node: Node, start: int, txt_with_dollar, new_active_node, new_remainder):
+        leaf_node = self.create_new_leaf(start)
+        start_index_char = ord(txt_with_dollar[start]) - self.ALPHABET_START
+        active_node.children[start_index_char] = leaf_node
+
+        # If the previous phase's pending node hasn't been resolved yet then the active node
+        # for this extension will resolve it
+        if self.pending_node is not None:
+            self.pending_node.suffix_link = active_node
+            self.pending_node = None
+
+        self.active_node = new_active_node
+        self.remainder = new_remainder
+
+    def create_new_leaf(self, start: int) -> Node:
+        return Node(start, self.global_end, isLeaf=True)
+
+    def create_new_internal_node(self, start: int, end: int) -> Node:
+        return Node(start, end, isLeaf=False)
+
+
+
