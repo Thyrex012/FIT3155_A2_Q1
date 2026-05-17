c62a6574bb96b627ebe5154d5959ad05f619af2f
Thyrex12 <ekrithyreachlay012@gmail.com>
2026-05-17 23:32:22 +1000

    modify the traversal algorithm to work with the edge data structure

--- a2q1.py
+++ a2q1.py
@@ -1,23 +1,42 @@
+# Ekrithyreach Lay
+# Student ID: 33698759
+
+#################################################################
+# Data structure to support the construction of the suffix tree
+#################################################################
 class End:
     def __init__(self, val=0):
         self.val = val
 
 class Node:
-    def __init__(self, suffix_link = None, isLeaf = False, isRoot = False):
+    def __init__(self, suffix_link = None, isLeaf = False, isRoot = False, suffix_index = -1):
         self.suffix_link: Node = suffix_link
         self.children = [None for _ in range(126-36+1)]
         self.isLeaf = isLeaf
         self.isRoot = isRoot
+        self.suffix_index = suffix_index
 
 class Edge:
-    def __init__(self, start: int, end: int, child_node: Node):
+    def __init__(self, start: int, end: int | End, child_node: Node):
         self.start = start
         self.end = end
         self.child_node = child_node
+    
+    def get_end_val(self):
+        if isinstance(self.end, End):
+            return self.end.val
+        else:
+            return self.end
+    
+    def get_length(self):
+        return self.get_end_val() - self.start + 1
 
+#########################
+#  Ukkonen's algorithm
+#########################
 class Ukkonen_algorithm:
     def __init__(self):
-        self.root = Node(-1, -1, isLeaf = False, isRoot= True)
+        self.root = Node(isLeaf = False, isRoot= True)
         self.root.suffix_link = self.root
         self.ALPHABET_START = 36
         self.ALPHABET_END = 126
@@ -34,10 +53,9 @@ class Ukkonen_algorithm:
         for i in range(n):
             self.global_end.val = i
             for j in range(last_j, i+1):
-                self.active_node, self.remainder = self.traverse(self.active_node, self.remainder)
+                self.active_node, self.remainder = self.traverse(self.active_node, self.remainder, txt_with_dollar)
                 if self.remainder == None and self.active_node.children[i+1] == None:
                     self.rule_two_alternate(self.active_node, , txt_with_dollar, i, self.remainder)
-                else if 
                 return
             
     def rule_two_regular(self, active_node: Node, remainder: tuple[int, int], txt_with_dollar, phase_i, new_remainder):
@@ -106,30 +124,26 @@ class Ukkonen_algorithm:
         #Performing skip counts until its no longer possible
         while skip_count_start <= remainder_end:
             start_char_index = ord(txt_with_dollar[skip_count_start]) - self.ALPHABET_START
-            curr_node = curr_node.children[start_char_index]
-            if curr_node.isLeaf == False:
-                curr_node_start = curr_node.start
-                curr_node_end = curr_node.end
-            else:
-                curr_node_start = curr_node.start
-                curr_node_end = curr_node.end.val
-            curr_node_length = curr_node_end - curr_node_start + 1
-
+            curr_edge_to_traverse = curr_node.children[start_char_index]
+            edge_length = curr_edge_to_traverse.get_length()
             remaining_length = remainder_end - skip_count_start + 1
-            if remaining_length <= curr_node_length:
-                break
+            #This would mean that the entire edge can be skipped over which allows us to get towards the next node instantly
+            if remaining_length > edge_length:
+                skip_count_start += edge_length
+                curr_node = curr_edge_to_traverse.child_node
+            # If the remaining length is smaller than the edge length then that means that we cant skip count further
+            # and return back the most recent node we're standing on along with the remaining remainder that hasn't been
+            # consumed
+            else:
+                return curr_node, (skip_count_start, remainder_end)
+        #This means that we have directly landed on a node and there is no more remainder
+        return curr_node, None
 
-            skip_count_start += curr_node_length
+    def create_new_leaf(self, suffix_index) -> Node:
+        return Node(isLeaf=True, suffix_index= suffix_index)
 
-        return curr_node, (skip_count_start, remainder_end)
-            
-        
-
-    def create_new_leaf(self, start: int) -> Node:
-        return Node(start, self.global_end, isLeaf=True)
-
-    def create_new_internal_node(self, start: int, end: int) -> Node:
-        return Node(start, end, isLeaf=False)
+    def create_new_internal_node(self) -> Node:
+        return Node()
     
 
 
